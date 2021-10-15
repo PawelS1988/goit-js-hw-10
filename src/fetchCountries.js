44366ad23fc718ff253a82a33e7da9427017a66b
PawelS1988 <szatkowskipawel1988@gmail.com>
2021-10-15 14:28:34 +0200

    work work

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,13 @@
-const fetchCountries
+const fetchCountries = name => {
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`,
+  ).then(response => {
+    console.log('fetchCountries response', response);
+    if (!response.ok) {
+      throw new Error('fetchCountries response error', response.status);
+    }
+    return response.json();
+  });
+};
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './css/styles.css';
+'use strict';
 
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('input#search-box');
+const countryList = document.querySelector('ul.country-list');
+const countryInfo = document.querySelector('div.country-info');
